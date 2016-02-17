481518cf144866517a52d1875eb8fcc2f5dad0e5
Paul Betts <paul@paulbetts.org>
2016-02-16 16:21:50 -0800

    Don't use a 2sec timeout in spawn-promise, wait until the pipes close

--- src/spawn-promise.js
+++ src/spawn-promise.js
@@ -20,28 +20,36 @@ export default function spawn(exe, params, opts=null) {
     } else {
       proc = spawnOg(exe, params, opts);
     }
+    
+    // We need to wait until all three events have happened:
+    // * stdout's pipe is closed
+    // * stderr's pipe is closed
+    // * We've got an exit code
+    let rejected = false;
+    let refCount = 3;
+    let release = () => { 
+      if (--refCount <= 0 && !rejected) resolve(stdout);
+    };
 
     let stdout = '';
     let bufHandler = (b) => {
       let chunk = b.toString();
       stdout += chunk;
     };
-
+    
     proc.stdout.on('data', bufHandler);
+    proc.stdout.once('close', release);
     proc.stderr.on('data', bufHandler);
+    proc.stderr.once('close', release);
     proc.on('error', (e) => reject(e));
 
     proc.on('close', (code) => {
-      // NB: Close fires before stdout fully flushes (i.e. you'll get 'data' 
-      // after 'close'), but there is no notification as to when data is fully
-      // flushed.
-      setTimeout(() => {
-        if (code === 0) {
-          resolve(stdout);
-        } else {
-          reject(new Error(`Failed with exit code: ${code}\nOutput:\n${stdout}`));
-        }      
-      }, 2000);
+      if (code === 0) {
+        release();
+      } else {
+        rejected = true;
+        reject(new Error(`Failed with exit code: ${code}\nOutput:\n${stdout}`));
+      }      
     });
   });
 }
