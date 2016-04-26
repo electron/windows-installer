a0f9ca24901ca7dfa6d620d63b6bea1c0f5dcd0a
develar <develar@gmail.com>
2016-04-26 08:12:50 +0200

    get rid of nuget
    Speed up 4x-8x

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import template from 'lodash.template';
 import spawn from './spawn-promise';
 import asar from 'asar';
 import path from 'path';
 import * as fsUtils from './fs-utils';
+import archiver from 'archiver';
+import * as fs from 'fs-extra';
+import { Promise } from 'bluebird';
 
-const log = require('debug')('electron-windows-installer:main');
+const log = require('debug')('electron-windows-installer');
 
 export function convertVersion(version) {
   const parts = version.split('-');
@@ -18,29 +20,13 @@ export function convertVersion(version) {
 }
 
 export async function createWindowsInstaller(options) {
-  let useMono = false;
-
-  const monoExe = 'mono';
-  const wineExe = 'wine';
-
-  if (process.platform !== 'win32') {
-    useMono = true;
-    if (!wineExe || !monoExe) {
-      throw new Error('You must install both Mono and Wine on non-Windows');
-    }
-
-    log(`Using Mono: '${monoExe}'`);
-    log(`Using Wine: '${wineExe}'`);
-  }
-
-  let { appDirectory, outputDirectory, loadingGif } = options;
-  outputDirectory = path.resolve(outputDirectory || 'installer');
+  const useMono = process.platform !== 'win32';
+  const { appDirectory } = options;
 
   const vendorPath = path.join(__dirname, '..', 'vendor');
-  const vendorUpdate = path.join(vendorPath, 'Update.exe');
   const appUpdate = path.join(appDirectory, 'Update.exe');
 
-  await fsUtils.copy(vendorUpdate, appUpdate);
+  await fsUtils.copy(path.join(vendorPath, 'Update.exe'), appUpdate);
   if (options.setupIcon && (options.skipUpdateIcon !== true)) {
     let cmd = path.join(vendorPath, 'rcedit.exe');
     let args = [
@@ -50,16 +36,11 @@ export async function createWindowsInstaller(options) {
 
     if (useMono) {
       args.unshift(cmd);
-      cmd = wineExe;
+      cmd = 'wine';
     }
 
     await spawn(cmd, args);
   }
-
-  const defaultLoadingGif = path.join(__dirname, '..', 'resources', 'install-spinner.gif');
-  loadingGif = loadingGif ? path.resolve(loadingGif) : defaultLoadingGif;
-
-  let {certificateFile, certificatePassword, remoteReleases, signWithParams, remoteToken} = options;
 
   const metadata = {
     description: '',
@@ -93,87 +74,34 @@ export async function createWindowsInstaller(options) {
     }
   }
 
-  metadata.owners = metadata.owners || metadata.authors;
-  metadata.version = convertVersion(metadata.version);
-  metadata.copyright = metadata.copyright ||
-    `Copyright © ${new Date().getFullYear()} ${metadata.authors || metadata.owners}`;
-
-  let templateData = await fsUtils.readFile(path.join(__dirname, '..', 'template.nuspec'), 'utf8');
-  if (path.sep === '/') {
-    templateData = templateData.replace(/\\/g, '/');
-  }
-  const nuspecContent = template(templateData)(metadata);
-
-  log(`Created NuSpec file:\n${nuspecContent}`);
-
-  const nugetOutput = await fsUtils.createTempDir('si-');
-  const targetNuspecPath = path.join(nugetOutput, metadata.name + '.nuspec');
-
-  await fsUtils.writeFile(targetNuspecPath, nuspecContent);
-
-  let cmd = path.join(vendorPath, 'nuget.exe');
-  let args = [
-    'pack', targetNuspecPath,
-    '-BasePath', appDirectory,
-    '-OutputDirectory', nugetOutput,
-    '-NoDefaultExcludes'
-  ];
-
-  if (useMono) {
-    args.unshift(cmd);
-    cmd = monoExe;
-  }
-
-  // Call NuGet to create our package
-  log(await spawn(cmd, args));
-  const nupkgPath = path.join(nugetOutput, `${metadata.name}.${metadata.version}.nupkg`);
-
-  if (remoteReleases) {
-    cmd = path.join(vendorPath, 'SyncReleases.exe');
-    args = ['-u', remoteReleases, '-r', outputDirectory];
+  const outputDirectory = path.resolve(options.outputDirectory || 'installer');
+  if (options.remoteReleases) {
+    let cmd = path.join(vendorPath, 'SyncReleases.exe');
+    let args = ['-u', options.remoteReleases, '-r', outputDirectory];
 
     if (useMono) {
       args.unshift(cmd);
-      cmd = monoExe;
+      cmd = 'mono';
     }
 
-    if (remoteToken) {
-      args.push('-t', remoteToken);
+    if (options.remoteToken) {
+      args.push('-t', options.remoteToken);
     }
 
-    log(await spawn(cmd, args));
+    await spawn(cmd, args);
   }
 
-  cmd = path.join(vendorPath, 'Update.com');
-  args = [
-    '--releasify', nupkgPath,
-    '--releaseDir', outputDirectory,
-    '--loadingGif', loadingGif
-  ];
-
-  if (useMono) {
-    args.unshift(path.join(vendorPath, 'Update-Mono.exe'));
-    cmd = monoExe;
+  // todo fix Squirrel.windows "Sharing violation on path" (avoid copy, use file directly)
+  const squirrelWorkaroundDir = path.join(outputDirectory, '.tmp');
+  await fsUtils.mkdirs(squirrelWorkaroundDir);
+  try {
+    const nupkgPath = path.join(squirrelWorkaroundDir, 'in.nupkg');
+    await pack(metadata, appDirectory, nupkgPath);
+    await releasify(nupkgPath, outputDirectory, options, vendorPath);
   }
-
-  if (signWithParams) {
-    args.push('--signWithParams');
-    args.push(signWithParams);
-  } else if (certificateFile && certificatePassword) {
-    args.push('--signWithParams');
-    args.push(`/a /f "${path.resolve(certificateFile)}" /p "${certificatePassword}"`);
+  finally {
+    await fsUtils.remove(squirrelWorkaroundDir);
   }
-
-  if (options.setupIcon) {
-    args.push('--setupIcon');
-    args.push(path.resolve(options.setupIcon));
-  }
-
-  if (options.noMsi) {
-    args.push('--no-msi');
-  }
-
-  log(await spawn(cmd, args));
 
   if (options.fixUpPaths !== false) {
     log('Fixing up paths');
@@ -194,4 +122,107 @@ export async function createWindowsInstaller(options) {
       }
     }
   }
+}
+
+function pack(metadata, appDirectory, outFile) {
+  return new Promise(function (resolve, reject) {
+    const archive = archiver('zip', {store: true});
+    const out = fs.createWriteStream(outFile);
+    out.on('close', function () {
+      resolve(outFile);
+    });
+    archive.on('error', reject);
+    archive.pipe(out);
+
+    archive.directory(appDirectory, 'lib/net45');
+
+    archive.append(`<?xml version="1.0"?>
+<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+  <Relationship Id="Re0" Target="/${metadata.name}.nuspec" Type="http://schemas.microsoft.com/packaging/2010/07/manifest"/>
+  <Relationship Id="Re1" Target="/package/services/metadata/core-properties/1.psmdcp" Type="http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties"/>
+</Relationships>`, {name: '.rels', prefix: '_rels'});
+
+    const author = metadata.authors || metadata.owners;
+    const copyright = metadata.copyright ||
+                      `Copyright © ${new Date().getFullYear()} ${author}`;
+    const version = convertVersion(metadata.version);
+    const nuspecContent = `<?xml version="1.0"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2011/08/nuspec.xsd">
+  <metadata>
+    <id>${metadata.name}</id>
+    <title>${metadata.title}</title>
+    <version>${version}</version>
+    <authors>${author}</authors>
+    <owners>${metadata.owners || metadata.authors}</owners>
+    <iconUrl>${metadata.iconUrl}</iconUrl>
+    <requireLicenseAcceptance>false</requireLicenseAcceptance>
+    <description>${metadata.description}</description>
+    <copyright>${copyright}</copyright>${metadata.extraMetadataSpecs || ''}
+  </metadata>
+</package>`;
+    log(`Created NuSpec file:\n${nuspecContent}`);
+
+    archive.append(nuspecContent, {name: metadata.name + '.nuspec'});
+
+    archive.append(`<?xml version="1.0"?>
+<Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
+  <Default ContentType="application/vnd.openxmlformats-package.relationships+xml" Extension="rels"/>
+  <Default ContentType="application/octet" Extension="nuspec"/>
+  <Default ContentType="application/octet" Extension="pak"/>
+  <Default ContentType="application/octet" Extension="asar"/>
+  <Default ContentType="application/octet" Extension="bin"/>
+  <Default ContentType="application/octet" Extension="dll"/>
+  <Default ContentType="application/octet" Extension="exe"/>
+  <Default ContentType="application/octet" Extension="dat"/>
+  <Default ContentType="application/vnd.openxmlformats-package.core-properties+xml" Extension="psmdcp"/>
+</Types>`, {name: '[Content_Types].xml'});
+
+    archive.append(`<?xml version="1.0"?>
+<coreProperties xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xmlns="http://schemas.openxmlformats.org/package/2006/metadata/core-properties">
+  <dc:creator>${author}</dc:creator>
+  <dc:description>${metadata.description}</dc:description>
+  <dc:identifier>${metadata.name}</dc:identifier>
+  <keywords/>
+  <lastModifiedBy>NuGet, Version=3.4.0.653, Culture=neutral, PublicKeyToken=31bf3856ad364e35;Unix 15.4.0.0;.NET Framework 4.5</lastModifiedBy>
+  <dc:title>${metadata.title}</dc:title>
+  <version>${version}</version>
+</coreProperties>`, {name: '1.psmdcp', prefix: 'package/services/metadata/core-properties'});
+
+    archive.finalize();
+  });
+}
+
+async function releasify(nupkgPath, outputDirectory, options, vendorPath) {
+  let cmd = path.join(vendorPath, 'Update.com');
+  const args = [
+    '--releasify', nupkgPath,
+    '--releaseDir', outputDirectory,
+    '--loadingGif', options.loadingGif ? path.resolve(options.loadingGif) : path.join(__dirname, '..', 'resources', 'install-spinner.gif')
+  ];
+
+  if (process.platform !== 'win32') {
+    args.unshift(path.join(vendorPath, 'Update-Mono.exe'));
+    cmd = 'mono';
+  }
+
+  const {certificateFile, certificatePassword, signWithParams} = options;
+  if (signWithParams) {
+    args.push('--signWithParams');
+    args.push(signWithParams);
+  } else if (certificateFile && certificatePassword) {
+    args.push('--signWithParams');
+    args.push(`/a /f "${path.resolve(certificateFile)}" /p "${certificatePassword}"`);
+  }
+
+  if (options.setupIcon) {
+    args.push('--setupIcon');
+    args.push(path.resolve(options.setupIcon));
+  }
+
+  if (options.noMsi) {
+    args.push('--no-msi');
+  }
+
+  await spawn(cmd, args);
 }
