412453ba483c256f06e975ca89ba60d083ace788
Brad Carter <brad@chartiq.com>
2019-02-21 17:27:49 -0500

    Update fs-extra to 6.0.1 This version of fs-extra has support for node 6.

--- src/fs-utils.js
+++ src/fs-utils.js
@@ -1,30 +1,21 @@
-import { copy as extraCopy } from 'fs-extra';
 import { Promise } from 'bluebird';
+import { copy, readFile, unlink, writeFile, rename, stat, pathExists, readdir } from 'fs-extra';
 import temp from 'temp';
-import fs from 'fs';
-
-const log = require('debug')('electron-windows-installer:fs-utils');
-
 temp.track();
 
-export const copy = Promise.promisify(extraCopy);
-export const createTempDir = Promise.promisify(temp.mkdir);
-export const readFile = Promise.promisify(fs.readFile);
-export const readDir = Promise.promisify(fs.readdir);
-export const unlink = Promise.promisify(fs.unlink);
-export const writeFile = Promise.promisify(fs.writeFile);
-export const rename = Promise.promisify(fs.rename);
+const createTempDir = Promise.promisify(temp.mkdir);
+const inspect = stat;
+const fileExists = pathExists;
+const readDir = readdir;
 
-const inspect = Promise.promisify(fs.stat);
-export async function fileExists(file) {
-  let stats;
-
-  try {
-    stats = await inspect(file);
-    return stats.isFile();
-  } catch(err) {
-    log(err);
-  }
-
-  return false;
-}
+export {
+  copy,
+  createTempDir,
+  readFile,
+  readDir,
+  unlink,
+  writeFile,
+  rename,
+  inspect,
+  fileExists
+};
