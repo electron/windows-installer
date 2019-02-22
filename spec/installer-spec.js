2ff3bca5fcc72eb38f97a4a70f55b16e0713a77a
Brad Carter <brad@chartiq.com>
2019-02-21 19:03:22 -0500

    Remove unnecessary methods in fs-utils in favor of fs-extras.

--- spec/installer-spec.js
+++ spec/installer-spec.js
@@ -1,6 +1,7 @@
 import test from 'ava';
 import path from 'path';
-import { createTempDir, fileExists, unlink, readDir } from '../src/fs-utils';
+import { createTempDir } from '../src/fs-utils';
+import { pathExists, unlink, readdir } from 'fs-extra';
 import { createWindowsInstaller } from '../src/index.js';
 
 const log = require('debug')('electron-windows-installer:spec');
@@ -10,7 +11,7 @@ const appDirectory = path.join(__dirname, 'fixtures/app');
 test.beforeEach(async () => {
   const updateExePath = path.join(appDirectory, 'Update.exe');
 
-  if (await fileExists(updateExePath)) {
+  if (await pathExists(updateExePath)) {
     await unlink(updateExePath);
   }
 });
@@ -26,15 +27,15 @@ test('creates a nuget package and installer', async t => {
   await createWindowsInstaller(options);
 
   log(`Verifying assertions on ${outputDirectory}`);
-  log(JSON.stringify(await readDir(outputDirectory)));
+  log(JSON.stringify(await readdir(outputDirectory)));
 
-  t.true(await fileExists(path.join(outputDirectory, 'myapp-1.0.0-full.nupkg')));
-  t.true(await fileExists(path.join(outputDirectory, 'MyAppSetup.exe')));
+  t.true(await pathExists(path.join(outputDirectory, 'myapp-1.0.0-full.nupkg')));
+  t.true(await pathExists(path.join(outputDirectory, 'MyAppSetup.exe')));
 
   if (process.platform === 'win32') {
-    t.true(await fileExists(path.join(outputDirectory, 'MyAppSetup.msi')));
+    t.true(await pathExists(path.join(outputDirectory, 'MyAppSetup.msi')));
   }
 
   log('Verifying Update.exe');
-  t.true(await fileExists(path.join(appDirectory, 'Update.exe')));
+  t.true(await pathExists(path.join(appDirectory, 'Update.exe')));
 });
--- src/fs-utils.js
+++ src/fs-utils.js
@@ -1,21 +1,9 @@
 import { Promise } from 'bluebird';
-import { copy, readFile, unlink, writeFile, rename, stat, pathExists, readdir } from 'fs-extra';
 import temp from 'temp';
 temp.track();
 
 const createTempDir = Promise.promisify(temp.mkdir);
-const inspect = stat;
-const fileExists = pathExists;
-const readDir = readdir;
 
 export {
-  copy,
-  createTempDir,
-  readFile,
-  readDir,
-  unlink,
-  writeFile,
-  rename,
-  inspect,
-  fileExists
+  createTempDir
 };
